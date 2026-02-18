fbe9728b07f0aa177bcd368dab8d86a8e499173c
tangerino4 <81923856+tangerino4@users.noreply.github.com>
2026-02-18 12:57:50 +0100

    Delete netlify.toml.js

--- netlify.toml.js
+++
@@ -1,7 +0,0 @@
-[build]
-  publish = "public"  # or your static folder
-[[redirects]]
-  from = "/api/coinbrain/:chain_id/:coin_address/trades"
-  to = "https://api.coinbrain.com/public/:chain_id/coin/:coin_address/trades"
-  status = 200
-  force = true
